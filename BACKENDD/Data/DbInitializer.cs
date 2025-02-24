db64991a373e0d84496fd963df5fae63a7a0d671
rangoffn <rangofn@bk.ru>
2025-02-24 10:00:25 +0300

    Добавьте файлы проекта.

---
+++ BACKENDD/Data/DbInitializer.cs
@@ -0,0 +1,24 @@
+﻿using BACKENDD.Models;
+using System.Linq;
+
+namespace BACKENDD.Data
+{
+    public static class DbInitializer
+    {
+        public static void Initialize(AppDbContext context)
+        {
+
+            // Если таблица Contacts не пуста, то выходим
+
+            // Иначе добавляем несколько начальных данных
+            var contacts = new Contact[]
+            {
+                new Contact { Name = "Иван", SecName = "Иванов", Age = 30, Email = "ivan@example.com", Message = "Привет!" },
+                new Contact { Name = "Мария", SecName = "Петрова", Age = 25, Email = "maria@example.com", Message = "Как дела?" }
+            };
+
+            context.Contacts.AddRange(contacts);
+            context.SaveChanges();  // Сохраняем изменения
+        }
+    }
+}
---
+++ BACKENDD/Models/Contact.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BACKENDD.Models
+{
+    public class Contact
+    {
+
+        [Key] 
+        public int Id { get; set; }  
+
+        [Display(Name = "Введите Имя")]
+        [Required(ErrorMessage = "НУЖНО ИМЯ!")]
+
+        public required string Name { get; set; }
+
+        [Display(Name = "Введите Фамилию")]
+        [Required(ErrorMessage = "НУЖНА ФАМИЛИЯ!")]
+
+
+        public required string SecName { get; set; }
+
+        [Display(Name = "Введите Возраст")]
+        [Required(ErrorMessage = "НУЖЕН ВОЗРАСТ!")]
+
+        public required int Age { get; set; }
+
+        [Display(Name = "Введите Почту")]
+        [Required(ErrorMessage = "НУЖНА ПОЧТА!")]
+
+        public required string Email { get; set; }
+
+        [Display(Name = "Введите Сообщение")]
+        [Required(ErrorMessage = "НУЖНО СООБЩЕНИЕ!")]
+
+        public required string Message { get; set; }
+
+
+
+
+
+    }
+}
